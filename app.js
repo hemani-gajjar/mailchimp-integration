c3875f46fea29418132fc6e45a987be9d1d6c166
Hemani Gajjar <hmngjjr17@gmail.com>
2022-06-06 20:26:20 +0530

    remove debug statements

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(
 );
 app.use(express.json());
 
-// console.log(process.env.API_KEY);
-// console.log(process.env.DATA_CENTER);
-// console.log(process.env.AUDIENCE_ID);
-
 //Bodyparser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -28,17 +24,11 @@ app.use(express.static(path.join(__dirname, "public")));
 //Submit Route
 app.post("/submit", (req, res) => {
   console.log("You just made a POST request to the route /submit");
-  console.log(req.body.array);
-  res.send("Hello there");
 
   for (let i = 1; i < req.body.array.length; i++) {
     if (req.body.array[i].length == 0) {
       continue;
     } else {
-      console.log(req.body.array[i][2]);
-      console.log(req.body.array[i][0]);
-      console.log(req.body.array[i][1]);
-
       const email = req.body.array[i][2];
       const firstName = req.body.array[i][0];
       const lastName = req.body.array[i][1];
