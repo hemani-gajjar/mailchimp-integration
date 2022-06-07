3361252f49f38b3a02322b8bf65160cabd093bae
Hemani Gajjar <hmngjjr17@gmail.com>
2022-06-07 19:48:02 +0530

    add create new campaign functions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fetch = require("node-fetch");
 const { PassThrough } = require("stream");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 require("dotenv").config();
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(
     extended: true,
   })
 );
+
 app.use(express.json());
 
 //Bodyparser Middleware
@@ -20,6 +22,70 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Static Folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Mailchimp Cofiguration
+mailchimp.setConfig({
+  apiKey: process.env.API_KEY,
+  server: process.env.DATA_CENTER,
+});
+
+// Check if able to make API calls to Mailchimp API
+async function run() {
+  const response = await mailchimp.ping.get();
+  console.log(response);
+}
+
+run();
+
+// Function to list all Campaigns
+const listCampaigns = async () => {
+  const response = await mailchimp.campaigns.list();
+  console.log(response);
+};
+
+let campaignID = "1a6d524c52";
+
+// Function to create a new Campaign
+const createCampaign = async () => {
+  const response = await mailchimp.campaigns.create({
+    type: "plaintext",
+    recipients: { list_id: process.env.AUDIENCE_ID },
+    settings: {
+      subject_line: "This is the Subject Line",
+      from_name: process.env.FROM_NAME,
+      title: "Test API Campaign",
+      reply_to: process.env.EMAIL,
+    },
+  });
+  console.log(response);
+  console.log(response.id);
+  campaignID = response.id; // Campaign ID of the newly created campaign
+};
+
+// createCampaign();
+console.log("Campaign ID : " + campaignID);
+
+// Get Campaign Content
+const getContent = async () => {
+  const response = await mailchimp.campaigns.getContent("2cea993018");
+  console.log(response);
+};
+
+// getContent();
+
+// Set content for the created Campaign
+const setContent = async () => {
+  const response = await mailchimp.campaigns.setContent(campaignID, {
+    data: {
+      message: "Some test Data",
+      plain_text:
+        "------ Hello There ------\n " + "This is a simple plaintext content",
+    },
+  });
+  console.log(response);
+};
+
+// setContent();
 
 //Submit Route
 app.post("/submit", (req, res) => {
@@ -66,6 +132,29 @@ app.post("/submit", (req, res) => {
   }
 });
 
-const port = process.env.PORT || 5000;
+// Send Route
+app.post("/send", (req, res) => {
+  console.log("You just made a POST request at the route /send");
+});
 
+const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server started on ${port}`));
+
+// // Add a contact to an Audience (Using AUDIENCE_ID)
+// async function addContact() {
+//   const response = await mailchimp.lists.addListMember(
+//     process.env.AUDIENCE_ID,
+//     {
+//       email_address: "test@getMaxListeners.com",
+//       status: "subscribed",
+//       merge_fields: {
+//         FNAME: "Ruby",
+//         LNAME: "Granger",
+//       },
+//     }
+//   );
+
+//   console.log(
+//     `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+//   );
+// }
